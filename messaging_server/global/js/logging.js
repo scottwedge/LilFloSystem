3b829d83291a377fdadb40d292af8909ee264ad3
Michael Sobrepera <mjsobrep@live.com>
2017-07-28 15:14:52 -0400

    reorg

---
+++ messaging_server/global/js/logging.js
@@ -0,0 +1,11 @@
+function trace(text) {
+  if (text[text.length - 1] === '\n') {
+    text = text.substring(0, text.length - 1);
+  }
+  if (window.performance) {
+    var now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now + ': ' + text);
+  } else {
+    console.log(text);
+  }
+}
