c02ff738be9e68dd8d40fe0f1eea8adc789312fb
Michael Sobrepera <mjsobrep@live.com>
2017-07-28 14:43:15 -0400

    got video up

--- messaging_server/client/js/client_socket.js
+++ messaging_server/client/js/client_socket.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var socket = io.connect('http://localhost:8080');
 
 var name = prompt('What is your username?');
@@ -12,3 +14,28 @@ socket.on('new_robot',function(name){
     alert('new robot connected: '+name);
 })
 
+
+/////// WebRTC Stuff
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: true
+};
+var video = document.querySelector('video');
+
+function successCallback(stream) {
+  window.stream = stream; // stream available to console
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
