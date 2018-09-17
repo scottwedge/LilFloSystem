6ac04d41b1635aeaf08eb34d421bf0dc4a7f6853
Michael Sobrepera <mjsobrep@live.com>
2018-09-17 11:27:15 -0400

    created pushing script

---
+++ pushToRobot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+## This file allows for the code updated on a dev computer to be pushed to 
+## the on robot NUC. It depends on the NUC being setup specifically as done in 
+## the README.
+
